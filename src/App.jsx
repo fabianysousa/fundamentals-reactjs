89cdab03e1d7d3dd64948862ec3d783ecce1a6d0
fabianysc <fabianydesousa@gmail.com>
2023-02-26 12:05:27 -0300

    :tada: feat: applying fundamentals of components and properties

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import {Post} from "./Post"
+
+function App() {
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <Post author="Fabiany de Sousa" context="Teste1"/>
+      <Post author="Anonimo" context="Teste2"/>
+    </div>  
+  )
+}
+
+export default App
